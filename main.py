7169b0ca83a8b99dc33eeb74083120ae91bdc063
Adrabi Abderrahim <59355459+adrabi-abderrahim@users.noreply.github.com>
2023-03-02 13:12:34 +0000

    Add use Jinja

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs 15,00,000'
+  }
+]
+
 @app.route('/')
 def hellp_app():
-  return '<h1>Hello App</h1>'
+  return render_template('home.html', jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
